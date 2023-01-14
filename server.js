c709bdaded289ef0c1e66f98b4c8a2bc84d9062f
Juan Quino <quinojuan@gmail.com>
2023-01-14 15:49:52 -0300

    otro error en port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const morgan = require("morgan");
 var cors = require('cors')
 
 
-const studentRoutes = require("./src/routes");
+const nfts = require("./src/routes");
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -12,8 +12,8 @@ app.use(cors()) // Use this after the variable declaration
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.use("/nft", studentRoutes);
+app.use("/nft", nfts);
 
-app.listen(process.env.PORT || port, () => {
-  console.log(`server listening on port: ${process.env.PORT || port}`);
+app.listen(port, () => {
+  console.log(`server listening on port: ${port}`);
 });
